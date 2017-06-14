e751f0ec4fa91477db649f97a4d10e506a69d385
Gowtham Genupur <gowtham.gourov@gmail.com>
2017-06-14 23:46:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gowthamg/classfinder/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.gowthamg.classfinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button search, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search = (Button) findViewById(R.id.search_btn);
+        register = (Button) findViewById(R.id.register_btn);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "search clicked", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "register clicked", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+    }
+
+
+
+
+}
